87ec21270b5958ead99102c34837d63a14480e22
SHIRLEY ESTEFANI MALDONADO SIMBAÑA <168391201+shimaldonado@users.noreply.github.com>
2026-05-15 00:35:39 -0500

    Delete epn-event-manager directory

--- epn-event-manager/src/modules/events/dto/create-event.dto.ts
+++
@@ -1,8 +0,0 @@
-export class CreateEventDto {
-  source: string;
-  entity: string;
-  action: string;
-  title: string;
-  description?: string;
-  payload?: Record<string, unknown>;
-}
--- epn-event-manager/src/modules/stats/stats.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StatsController } from './stats.controller';
-import { EventsModule } from '../events/events.module';
-
-@Module({
-  imports: [EventsModule],
-  controllers: [StatsController],
-})
-export class StatsModule {}
